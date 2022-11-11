c737240a75985ad540b32d128f329d153d1896e6
myApp <kiril.ebarev@gmail.com>
2022-11-11 18:58:56 +0500

    clean code

--- pizza-ts/pizza-shop/src/App.tsx
+++ pizza-ts/pizza-shop/src/App.tsx
@@ -1,118 +1,87 @@
-import React, { useEffect, useRef, useState } from "react";
-import { AppLogo } from "./tsx/appBar/appLogo";
-import { AppMenu } from "./tsx/appBar/appMenu";
-import { AppSign } from "./tsx/appBar/appSing";
-import { CardPizza } from "./components/cardPizza/cardPizza";
-import { AboutAs } from "./tsx/footer/footer";
-import { AppBarFind } from "./tsx/appBar/appBarFind/appBarFind";
-import { AppBarBuy } from "./tsx/appBar/appBarBuy";
-import { useAppDispatch, useAppSelector } from "./hook";
-// import { fetchPizza } from "./redux/slices/apiSlice";
-import { IListPizza } from "./components/cardPizza/cardPizzaInterface/cardPizzaInterface";
-import { requestCardPizza } from "./API/offlinePizza/offlineRequest";
-// import { transforICardToIList } from "./logic/transforICardToIList";
+/** @format */
+
+import React, {useEffect, useRef, useState} from "react";
+import {AppLogo} from "./tsx/appBar/appLogo";
+import {AppMenu} from "./tsx/appBar/appMenu";
+import {AppSign} from "./tsx/appBar/appSing";
+import {CardPizza} from "./components/cardPizza/cardPizza";
+import {AboutAs} from "./tsx/footer/footer";
+import {AppBarFind} from "./tsx/appBar/appBarFind/appBarFind";
+import {AppBarBuy} from "./tsx/appBar/appBarBuy";
+import {useAppSelector} from "./hook";
 
 export const App: React.FC = () => {
-  // const dispatch = useAppDispatch();
-  // const { loading, error } = useAppSelector((state) => state.api);
-  const pizzaListAfterFiltred = useAppSelector((state) => state.api.pizzaList);
-  // const [pizzaList, setPizzaList] = useState<IListPizza[]>([]);
+    const pizzaListAfterFiltred = useAppSelector(
+        (state) => state.api.pizzaList
+    );
 
-  // useEffect(() => {
-  //   dispatch(fetchPizza());
-  //   console.log('first')
-  //   // transforICardToIList(pizzaListAfterFiltred);
-  // }, []);
+    const refHome = useRef<HTMLElement>(null);
+    const refPizza = useRef<HTMLDivElement>(null);
+    const refFooter = useRef<HTMLElement>(null);
 
-  // useEffect(() => {
-  //   const Debounce = setTimeout(() => {
-  //     console.log('second');
-  //     setPizzaList(
-  //       pizzaListAfterFiltred.map((items) => {
-  //         console.log("map");
-  //         let _ = {
-  //           id: items.id,
-  //           title: items.title,
-  //           imgPizza: items.imgPizza,
-  //           description: items.description,
-  //           price: items.price,
-  //           count: 1,
-  //           size: "md",
-  //         };
-  //         return _;
-  //       })
-  //     ); 
-  //   }, 500);
-  //   return () => clearTimeout(Debounce);
-  // }, [dispatch]);
+    const goToElement = (refElement: React.RefObject<HTMLElement>) => {
+        if (refElement && refElement.current) {
+            refElement.current.scrollIntoView({behavior: "smooth"});
+        }
+    };
 
-  const refHome = useRef<HTMLElement>(null);
-  const refPizza = useRef<HTMLDivElement>(null);
-  const refFooter = useRef<HTMLElement>(null);
+    return (
+        <div className='container mx-auto p-5 flex flex-col min-h-full'>
+            <div className='flex flex-col min-h-screen'>
+                <header
+                    ref={refHome}
+                    className='flex place-items-center justify-between'>
+                    <AppLogo />
+                    <AppBarFind />
+                    <AppSign />
+                </header>
+                <div className='flex items-center justify-between'>
+                    <nav className='flex items-center justify-start  space-x-5 mt-7 place-content-between'>
+                        <AppMenu
+                            refElement={refHome}
+                            goToElement={goToElement}
+                            name={"Home"}
+                        />
+                        <AppMenu
+                            refElement={refPizza}
+                            goToElement={goToElement}
+                            name={"Pizza"}
+                        />
+                        <AppMenu
+                            refElement={refFooter}
+                            goToElement={goToElement}
+                            name={"About us"}
+                        />
+                    </nav>
+                    <AppBarBuy />
+                </div>
 
-  const goToElement = (refElement: React.RefObject<HTMLElement>) => {
-    if (refElement && refElement.current) {
-      refElement.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  return (
-      <div className='container mx-auto p-5 flex flex-col min-h-full'>
-          <div className='flex flex-col min-h-screen'>
-              <header
-                  ref={refHome}
-                  className='flex place-items-center justify-between'>
-                  <AppLogo />
-                  <AppBarFind />
-                  <AppSign />
-              </header>
-              <div className='flex items-center justify-between'>
-                  <nav className='flex items-center justify-start  space-x-5 mt-7 place-content-between'>
-                      <AppMenu
-                          refElement={refHome}
-                          goToElement={goToElement}
-                          name={"Home"}
-                      />
-                      <AppMenu
-                          refElement={refPizza}
-                          goToElement={goToElement}
-                          name={"Pizza"}
-                      />
-                      <AppMenu
-                          refElement={refFooter}
-                          goToElement={goToElement}
-                          name={"About us"}
-                      />
-                  </nav>
-                  <AppBarBuy />
-              </div>
-
-              <body className='flex-auto mb-10'>
-                  <p ref={refPizza} className='mt-7 font-sans text-3xl'>
-                      Pizza
-                  </p>
-                  {/* 
+                <body className='flex-auto mb-10'>
+                    <p ref={refPizza} className='mt-7 font-sans text-3xl'>
+                        Pizza
+                    </p>
+                    {/* 
           {loading && <h2>Loading...</h2>}
           {error && <h2>An error occured: {error}</h2>} */}
-                  {pizzaListAfterFiltred.length !== 0 ? (
-                      <div className='mt-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2'>
-                          {pizzaListAfterFiltred.map((templateCardPizza) => (
-                              <CardPizza
-                                  templateCardPizza={templateCardPizza}
-                                  key={templateCardPizza.id}
-                              />
-                          ))}
-                      </div>
-                  ) : (
-                      <span className=' mt-10 flex items-center justify-center font-sans text-3xl'>
-                          We could not find the pizza you requested, try again
-                      </span>
-                  )}
-              </body>
-              <footer ref={refFooter} className='w-full bottom-0'>
-                  <AboutAs />
-              </footer>
-          </div>
-      </div>
-  );
+                    {pizzaListAfterFiltred.length !== 0 ? (
+                        <div className='mt-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2'>
+                            {pizzaListAfterFiltred.map((templateCardPizza) => (
+                                <CardPizza
+                                    templateCardPizza={templateCardPizza}
+                                    key={templateCardPizza.id}
+                                />
+                            ))}
+                        </div>
+                    ) : (
+                        <span className=' mt-10 flex items-center justify-center font-sans text-3xl'>
+                            We could not find the pizza you requested, try again
+                        </span>
+                    )}
+                </body>
+                <footer ref={refFooter} className='w-full bottom-0'>
+                    <AboutAs />
+                </footer>
+            </div>
+        </div>
+    );
 };
--- pizza-ts/pizza-shop/src/redux/slices/apiSlice.ts
+++ pizza-ts/pizza-shop/src/redux/slices/apiSlice.ts
@@ -1,40 +1,23 @@
 /** @format */
 
-import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
 import type {PayloadAction} from "@reduxjs/toolkit";
-import {
-    IcardPizza,
-    IListPizza,
-} from "../../components/cardPizza/cardPizzaInterface/cardPizzaInterface";
+import {IListPizza} from "../../components/cardPizza/cardPizzaInterface/cardPizzaInterface";
 import {requestCardPizza} from "../../API/offlinePizza/offlineRequest";
 
 export interface ApiState {
     pizzaList: IListPizza[];
     valueSearch: string;
-    loading: boolean;
-    error: string | null;
+    // loading: boolean;
+    // error: string | null;
 }
 
 const initialStateSearch: ApiState = {
     pizzaList: requestCardPizza,
     valueSearch: "",
-    loading: false,
-    error: null,
+    // loading: false,
+    // error: null,
 };
-
-// export const fetchPizza = createAsyncThunk<
-//   IListPizza[],
-//   undefined,
-//   { rejectValue: string }
-// >("pizzaApi/fetchPizzaApi", async function (_, { rejectWithValue }) {
-//   const response = await fetch("http://localhost:8000/api/pizza/");
-//   if (!response.ok) {
-//     return rejectWithValue("Server Error!");
-//   }
-
-//   const data:IListPizza[] = await response.json();
-//   return data;
-// });
 
 export const searchSlice = createSlice({
     name: "pizzaApi",
@@ -55,17 +38,6 @@ export const searchSlice = createSlice({
             state.valueSearch = action.payload;
         },
     },
-    // extraReducers: (builder) => {
-    //   builder
-    //     .addCase(fetchPizza.pending, (state) => {
-    //       state.loading = true;
-    //       state.error = null;
-    //     })
-    //     .addCase(fetchPizza.fulfilled, (state, action) => {
-    //       state.pizzaList = action.payload;
-    //       state.loading = false;
-    //     });
-    // },
 });
 
 // Action creators are generated for each case reducer function
